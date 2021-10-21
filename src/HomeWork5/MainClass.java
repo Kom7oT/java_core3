04706547b2a2321f11c51470e4df6e67c2fd1c23
Kom7oT <86073918+Kom7oT@users.noreply.github.com>
2021-10-21 19:37:40 +0300

    hw5

--- src/HomeWork5/MainClass.java
+++ src/HomeWork5/MainClass.java
@@ -1,19 +1,41 @@
 package HomeWork5;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    public static int CARS_COUNT = 4;
+    private static ExecutorService executorService = Executors.newFixedThreadPool(CARS_COUNT);
+    public static final AtomicInteger finishCount = new AtomicInteger(0);
+
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT + 1,
+                () -> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"));
+        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl, finishCount);
         }
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            executorService.execute(cars[i]);
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
-    }
-}
+        try {
+            cb.await();
+        } catch (InterruptedException | BrokenBarrierException ex) {
+            ex.printStackTrace();
+        }
 
+        try {
+            cdl.await();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            executorService.shutdown();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        }
+    }
+
+}
